a6bdce2e787301f2a1f1053de60c12f15ef96261
Alan B <a.brevick@techsmith.com>
2022-04-14 08:23:40 -0400

    ignore powerlevel10k in ohmyzsh

--- bin/flux-kustomize-check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# run docker command to check kustomize with proper folders mounted
-# on mac, requires gsed from brew. sed from normal linux would work.
-# run from a directory under kustomize containing a kustomization.yaml
-# copy to your ~/bin/ folder for ease of use
-d=`echo ${PWD} | gsed 's/.*\/kustomize\/\(.*\)/\1/'`
-kustomize_folder=` echo ${PWD} | gsed 's/\(.*\/kustomize\/\).*/\1/' `
-docker run --rm --entrypoint "" -v ${kustomize_folder}:/home/flux docker.io/fluxcd/flux:1.22.2 bash -c "cd $d; kustomize build ." 
